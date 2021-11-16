088ef486836b0301f9d86743f0cc2be05a6d4528
HUGUET Zachary <huguzac@ndlp.fr>
2021-11-16 12:55:34 +0100

    fin de cours

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/home", name="default")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
